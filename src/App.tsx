4fae66dfa4cf1783620d49004559c090af441213
Vitorbite <vitorbitencourt20@gmail.com>
2025-04-28 22:50:19 -0300

    Update

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  console.log("Oi");
+
+  return (
+    <div>
+      <h1>Olá Mundo</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        voluptatum fugiat adipisci, ab blanditiis ut illo aut iste quis est
+        dolores amet ea sapiente veritatis. Debitis suscipit laborum molestiae
+        ducimus.
+      </p>
+    </div>
+  );
+}
+
+export default App;
