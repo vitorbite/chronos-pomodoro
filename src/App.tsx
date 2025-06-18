3f9a1a9f8c9b5d5ba38ad8bdb9523a7fae400231
Vitorbite <vitorbitencourt20@gmail.com>
2025-06-18 15:43:19 -0300

    Atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import { TimerIcon } from "lucide-react";
+//import { TimerIcon } from "lucide-react";
 import { Heading } from "./components/Heading";
 
-import './styles/global.css';
-import './styles/theme.css';
+import "./styles/global.css";
+import "./styles/theme.css";
 
 export function App() {
-
   return (
     <>
-      <Heading>
-        Olá Mundo 1
-        <button>
-          <TimerIcon/>
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-        voluptatum fugiat adipisci, ab blanditiis ut illo aut iste quis est
-        dolores amet ea sapiente veritatis. Debitis suscipit laborum molestiae
-        ducimus.
-      </p>
+      <div className="container">
+        <div className="content">
+          <section><Heading>LOGO</Heading></section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
