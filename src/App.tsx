9434adb7197ca5c214909bcad73d166edb80f9c4
vitorbite <vitorbitencourt20@gmail.com>
2025-12-31 19:24:00 -0300

    Adicionando o componente Cycles

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
 import { DefaultInput } from "./components/Input";
+import { Cycles } from "./components/Cycles"
 
 import "./styles/global.css";
 import "./styles/theme.css";
@@ -37,8 +38,7 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0 0</p>
+            <Cycles/>
           </div>
 
           <div className="formRow">
