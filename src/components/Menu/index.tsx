035171575efdf9770cfd4ff87637bb549a999233
vitorbite <vitorbitencourt20@gmail.com>
2026-01-03 20:59:24 -0300

    Adicionando a mudança de tema claro e escuro

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,59 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
-  return <nav className={styles.menu}>
-    <a className= {styles.menuLink} href="#">
-      <HouseIcon />
-    </a>
-    
-    <a className= {styles.menuLink} href="#">
-      <HistoryIcon />
-    </a>
-    
-    <a className= {styles.menuLink} href="#">
-      <SettingsIcon />
-    </a>
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+  
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,){
 
-    <a className= {styles.menuLink} href="#">
-      <SunIcon />
-    </a>
-    </nav>;
+    event.preventDefault();
+
+    setTheme((prevTheme)=>{
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+  useEffect(()=>{
+    document.documentElement.setAttribute('data-theme', theme);
+  },[theme]);
+  return (
+    <nav className={styles.menu}>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para Home"
+        title="Ir para Home"
+      >
+        <HouseIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ver Histórico"
+        title="Ver Histórico"
+      >
+        <HistoryIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Configurações"
+        title="Configurações"
+      >
+        <SettingsIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Mudar Tema"
+        title="Mudar Tema"
+        onClick={(event)=>{handleThemeChange(event)}}
+      >
+        <SunIcon />
+      </a>
+    </nav>
+  );
 }
